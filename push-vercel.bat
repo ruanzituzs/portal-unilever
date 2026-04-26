395f104c85587efb83214ae3262290fec3b9b350
Unilever Dev <dev@unilever.local>
2026-04-26 09:43:07 -0300

    feat: add _redirects for netlify and update assets in angular.json

--- push-vercel.bat
+++ push-vercel.bat
@@ -1,4 +1,4 @@
-git add frontend/package.json frontend/angular.json frontend/.npmrc vercel.json frontend/vercel.json
+git add frontend/package.json frontend/angular.json frontend/.npmrc vercel.json frontend/vercel.json frontend/src/_redirects
 git commit -m "fix: remove packageManager lock, add zeroConfig false for vercel build"
 git push vercel-app main --force
 git push origin main
